64d2be6d51f44f1333d4cf55159f177ec3d03265
Manuel Alcocer Jiménez <manuel@alcocer.net>
2018-02-13 22:54:39 +0100

    last

--- pdns/migracion/mydns2pdns.sql
+++ pdns/migracion/mydns2pdns.sql
@@ -13,14 +13,14 @@ delimiter //
 
 CREATE OR REPLACE PROCEDURE limpia()
 BEGIN
-    DELETE FROM pdns.domains WHERE id > 5;
-    ALTER TABLE pdns.domains AUTO_INCREMENT = 7;
+    DELETE FROM pdns.domains;
+    ALTER TABLE pdns.domains AUTO_INCREMENT = 1;
 
-    DELETE FROM pdns.zones WHERE domain_id > 5;
-    ALTER TABLE pdns.zones AUTO_INCREMENT = 7;
+    DELETE FROM pdns.zones;
+    ALTER TABLE pdns.zones AUTO_INCREMENT = 1;
 
-    DELETE FROM pdns.records WHERE domain_id > 5;
-    ALTER TABLE pdns.records AUTO_INCREMENT = 7;
+    DELETE FROM pdns.records;
+    ALTER TABLE pdns.records AUTO_INCREMENT = 1;
 END
 //
 
@@ -29,7 +29,7 @@ CREATE OR REPLACE PROCEDURE check_name_record (p_origin VARCHAR(255),
                                                OUT p_act INTEGER)
 BEGIN
     set p_act := 0;
-    IF LENGTH(p_name) = 1 AND p_name = '@' THEN
+    IF (LENGTH(p_name) = 1 AND p_name = '@') OR LENGTH(p_name) = 0 THEN
         SELECT CONCAT(p_origin, '.') INTO p_name;
     ELSEIF LENGTH(p_name) = 1 AND p_name = '.' THEN
         set p_act := 1;
@@ -71,7 +71,7 @@ BEGIN
 
     DECLARE c_records CURSOR FOR
         SELECT name, type, data, aux, ttl, UNIX_TIMESTAMP(NOW())
-        FROM furanetdns.rr
+        FROM furanet.rr
             WHERE zone = p_zone_id;
 
     DECLARE CONTINUE HANDLER
@@ -87,7 +87,7 @@ BEGIN
             LEAVE get_records;
         END IF;
 
-        CALL check_name (p_origin, v_name, v_act);
+        CALL check_name_record (p_origin, v_name, v_act);
         CALL check_content (v_type, p_origin, v_content);
 
         IF v_act = 0 THEN
@@ -123,7 +123,7 @@ BEGIN
            CONCAT_WS(' ', ns, mbox, serial, refresh, retry, expire, minimum, ttl),
            ttl, active, UNIX_TIMESTAMP(NOW())
     INTO p_name, v_content, v_ttl, v_active, v_change_date
-    FROM furanetdns.soa
+    FROM furanet.soa
     WHERE id = p_zone_id;
 
     IF v_active = 'N' THEN
@@ -145,7 +145,6 @@ BEGIN
     SELECT COUNT(*) INTO v_domain_id FROM pdns.zones
         WHERE domain_id = p_domain_id;
 
-    select v_domain_id, p_domain_id;
     IF v_domain_id = 0 THEN
         INSERT INTO pdns.zones (domain_id, owner) VALUES (p_domain_id, 1);
     END IF;
@@ -157,7 +156,7 @@ CREATE OR REPLACE PROCEDURE insert_domain (p_zone_id INTEGER,
 BEGIN
     DECLARE v_name VARCHAR(255);
 
-    SELECT TRIM(TRAILING '.' FROM origin) INTO v_name FROM furanetdns.soa
+    SELECT TRIM(TRAILING '.' FROM origin) INTO v_name FROM furanet.soa
         WHERE id = p_zone_id;
 
     SELECT COUNT(*) INTO p_domain_id FROM pdns.domains
@@ -176,25 +175,33 @@ CREATE OR REPLACE PROCEDURE clone_zone (p_zone_id INTEGER)
 BEGIN
     DECLARE v_domain_id INTEGER;
     DECLARE v_name VARCHAR(255);
+    DECLARE insertando VARCHAR(255);
 
     CALL insert_domain (p_zone_id, v_domain_id);
     CALL insert_zone (v_domain_id);
     CALL clone_soa (p_zone_id, v_domain_id, v_name);
+    SET insertando := v_name;
+    SELECT insertando, v_domain_id;
     CALL clone_records (p_zone_id, v_domain_id, v_name);
 END
 //
 
-CREATE OR REPLACE PROCEDURE walk_domains (p_limite INT)
+CREATE OR REPLACE PROCEDURE walk_domains (INOUT p_limite INT)
 BEGIN
     DECLARE v_zone_id INTEGER;
     DECLARE v_done INT DEFAULT FALSE;
     DECLARE i INTEGER DEFAULT 0;
 
     DECLARE c_domains CURSOR FOR
-        SELECT id FROM furanetdns.soa;
+        SELECT id FROM furanet.soa;
 
     DECLARE CONTINUE HANDLER
         FOR NOT FOUND SET v_done = TRUE;
+
+    IF p_limite = 0 THEN
+        SELECT COUNT(id) INTO p_limite
+        FROM furanet.soa;
+    END IF;
 
     OPEN c_domains;
     get_domains: LOOP
@@ -206,7 +213,6 @@ BEGIN
         END IF;
 
         set i := i + 1;
-
         CALL clone_zone (v_zone_id);
 
     END LOOP;
@@ -215,13 +221,13 @@ BEGIN
 END
 //
 
-CREATE OR REPLACE PROCEDURE mydns2pdns()
+CREATE OR REPLACE PROCEDURE mydns2pdns(p_limite INTEGER)
 BEGIN
-    CALL walk_domains(50);
+    CALL walk_domains(p_limite);
 END
 //
 
 
 delimiter ;
 
-call mydns2pdns;
+-- call mydns2pdns();
